a2aebdbef999bb2dcedf442f21e54a46f33b08be
fixa03 <fixa03@gmail.com>
2022-05-11 11:58:29 +0400

    Добавьте файлы проекта.

---
+++ DH/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using lib1;
+
+namespace DH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person("Alice");
+            Person p2 = new Person("Bob");
+
+            p2.ListenG( p1.SayG() );
+            p2.ListenP( p1.SayP() );
+            p2.Listen( p1.SayPublicN() );
+            p1.Listen( p2.SayPublicN() );
+
+            p1.GenerateKey(); p2.GenerateKey();
+            p1.show();  p2.show();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ DH/lib1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace lib1
+{
+    class MathOperations
+    {
+		Random rnd;
+
+		public MathOperations()
+		{ rnd = new Random(); }
+
+		public bool IsPrime(int a)
+		{
+			//int count = 2;
+			for (int i = 2; i < a; i++)
+			{
+				if (a % i == 0)
+					return false;
+			}
+
+
+			return true;
+		}
+
+		public int GeneratePrime(int min = 3, int max = 100)
+		{
+			Thread.Sleep(200);
+
+			int res = rnd.Next(min, max);
+
+			while (!IsPrime(res))
+			{
+				res = rnd.Next(min, max);
+			}
+
+			return res;
+		}
+
+	}
+
+	class Person
+	{
+		string name;
+
+		int g, p;
+		int public_number_neighbor;
+		int private_number;
+		int key;
+
+		public Person(string name)
+		{ 
+			this.name = name;
+
+			MathOperations obj = new MathOperations();
+
+			g = obj.GeneratePrime();
+			p = obj.GeneratePrime();
+			private_number = obj.GeneratePrime();
+
+		}
+
+		public int SayG()
+		{ return g; }
+
+		public void ListenG(int g)
+		{ this.g = g; }
+
+		public int SayP()
+		{ return p; }
+
+		public void ListenP(int p)
+		{ this.p = p; }
+
+		public int SayPublicN()
+		{
+			return (int)BigInteger.ModPow(g, private_number, p);
+		}
+
+		public void Listen(int neighbor_number)
+		{
+			public_number_neighbor = neighbor_number;
+		}
+
+		public void GenerateKey()
+		{
+			key = (int)BigInteger.ModPow(public_number_neighbor, private_number, p);
+		}
+
+		public void show()
+        {
+			Console.WriteLine($"g: {g} p: {p}");
+			Console.WriteLine($"PrivateN: {private_number} NeighborP: {public_number_neighbor}");
+			Console.WriteLine($"Key: {key}");
+		}
+	
+	}
+}
